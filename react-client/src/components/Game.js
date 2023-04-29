caaaac4ca406287bc8740458339963e27d56d963
Miriam Binyamines <89273650+MiriamBin@users.noreply.github.com>
2023-04-29 23:34:25 +0300

    added useEffect to Game

--- react-client/src/components/Game.js
+++ react-client/src/components/Game.js
@@ -1,43 +1,47 @@
 import React, {useState} from "react";
 import Guess from "./Guess";
-
-
-function generateRandomNumbers() {
-    const arr = [];
-    while (arr.length < 4) {
-        const randomNum = Math.floor(Math.random() * 10).toString();
-        if (!arr.includes(randomNum)) {
-            arr.push(randomNum);
-        }
-    }
-    return arr;
-}
-
-    let target = generateRandomNumbers();
-    console.log("target", target);
+import {useEffect} from "react";
 
 function Game({setScore, setGameOver}){
     const [message, setMessage] = useState("");
-
     const [guess, setGuess] = useState(["", "", "", ""]);
     const [bulls, setBulls] = useState(0);
     const [cows, setCows] = useState(0);
+    const [target, setTarget] = useState([]);
+
+    useEffect(() => {
+        // This function will only run once, at the beginning of the program
+        setTarget(generateRandomNumbers());
+    }, []);
+
+    function generateRandomNumbers() {
+        const arr = [];
+        while (arr.length < 4) {
+            const randomNum = Math.floor(Math.random() * 10).toString();
+            if (!arr.includes(randomNum)) {
+                arr.push(randomNum);
+            }
+        }
+        console.log(arr);
+        return arr;
+    }
+
+
+
 
     return (
-            <Guess numbers={guess}
-                   setNumbers={setGuess}
-                   message = {message}
-                   setMessage = {setMessage}
-                   target = {target}
-                   cows = {cows}
-                   setCows = {setCows}
-                   bulls = {bulls}
-                   setBulls = {setBulls}
-                   setGameOver = {setGameOver}
-                   setScore = {setScore}
-            />
+        <Guess numbers={guess}
+               setNumbers={setGuess}
+               message = {message}
+               setMessage = {setMessage}
+               target = {target}
+               cows = {cows}
+               setCows = {setCows}
+               bulls = {bulls}
+               setBulls = {setBulls}
+               setGameOver = {setGameOver}
+               setScore = {setScore}
+        />
     );
 }
 export default Game;
-
-
