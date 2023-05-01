09649c10c891e302ae4265cd544db4da7d6c55ec
SalomonDemma <dennasa@edu.hac.ac.il>
2023-05-01 14:22:32 +0300

    something

--- react-client/src/components/Game.js
+++ react-client/src/components/Game.js
@@ -1,51 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import Guess from "./Guess";
 
-function Game({ setScore, setGameOver, setNewGame, newGame }) {
-    const [message, setMessage] = useState("");
-    const [guess, setGuess] = useState(["", "", "", ""]);
-    const [lastGuessesArr, setLastGuessesArr] = useState([]);
-    const [target, setTarget] = useState(generateRandomNumbers());
 
-    useEffect(() => {
-        if (newGame) {
-            setGuess(["", "", "", ""]);
-            setLastGuessesArr([]);
-            setTarget(generateRandomNumbers());
-            console.log(target)
-            setNewGame(false);
-            setGameOver(false);
-            setScore(0);
+function generateRandomNumbers() {
+    const arr = [];
+    while (arr.length < 4) {
+        const randomNum = Math.floor(Math.random() * 10).toString();
+        if (!arr.includes(randomNum)) {
+            arr.push(randomNum);
         }
-    }, [newGame, setNewGame, setGameOver, setScore, target, setTarget ]);
-
-    //console.log(target) //TODO: change place.
-    function generateRandomNumbers() {
-        const arr = [];
-        while (arr.length < 4) {
-            const randomNum = Math.floor(Math.random() * 10).toString();
-            if (!arr.includes(randomNum)) {
-                arr.push(randomNum);
-            }
-        }
-        return arr;
     }
-
-    return (
-        <>
-            <Guess
-                guess={guess}
-                setGuess={setGuess}
-                message={message}
-                setMessage={setMessage}
-                lastGuessesArr={lastGuessesArr}
-                setLastGuessesArr={setLastGuessesArr}
-                target={target}
-                setGameOver={setGameOver}
-                setScore={setScore}
-            />
-        </>
-    );
+    return arr;
 }
 
+    let target = generateRandomNumbers();
+    console.log("target", target);
+
+function Game({setScore, setGameOver}){
+    const [message, setMessage] = useState("");
+
+    const [guess, setGuess] = useState(["", "", "", ""]);
+    const [bulls, setBulls] = useState(0);
+    const [cows, setCows] = useState(0);
+
+    return (
+            <Guess numbers={guess}
+                   setNumbers={setGuess}
+                   message = {message}
+                   setMessage = {setMessage}
+                   target = {target}
+                   cows = {cows}
+                   setCows = {setCows}
+                   bulls = {bulls}
+                   setBulls = {setBulls}
+                   setGameOver = {setGameOver}
+                   setScore = {setScore}
+            />
+    );
+}
 export default Game;
+
+
